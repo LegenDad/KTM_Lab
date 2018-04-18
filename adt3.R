0a5910b64872f6e3070bf3a064a484c1a1bc7d04
LegenDad <hj.jedonald@gmail.com>
2018-04-18 13:53:05 +0900

    xgboost0.9

--- adt3.R
+++ adt3.R
@@ -1,3 +1,4 @@
+##### Basic #####
 adt <- read.csv("Data/AD_Tracking/train_sample.csv", stringsAsFactors = F)
 adt$click_time <- as.POSIXct(adt$click_time)
 range(adt$click_time)
@@ -15,16 +16,16 @@ colnames(adt)[11:15] <- c("ip_hw", "ip_app", "ip_dev", "ip_os", "ip_ch")
 colnames(adt)
 head(adt)
 
-adt$ip_hw <- as.factor(adt$ip_hw)
-adt$ip_app <- as.factor(adt$ip_app)
-adt$ip_dev <- as.factor(adt$ip_dev)
-adt$ip_os <- as.factor(adt$ip_os)
-adt$ip_ch <- as.factor(adt$ip_ch)
-adt$is_attributed <- as.factor(adt$is_attributed)
-head(adt)
+#adt$ip_hw <- as.factor(adt$ip_hw)
+#adt$ip_app <- as.factor(adt$ip_app)
+#adt$ip_dev <- as.factor(adt$ip_dev)
+#adt$ip_os <- as.factor(adt$ip_os)
+#adt$ip_ch <- as.factor(adt$ip_ch)
+#adt$is_attributed <- as.factor(adt$is_attributed)
+#head(adt)
 #install.packages("caret")
 library(caret)
-args(createDataPartition)
+#args(createDataPartition)
 colnames(adt)
 adt_index<- createDataPartition(adt$is_attributed, p=0.7, list = F)
 adtr <- adt[adt_index,]
@@ -105,10 +106,8 @@ auc3 <- performance(pr3, measure = "auc")
 auc3 <- auc3@y.values[[1]]
 auc3
 ##### XGBOOST #####
-rm(list=ls())
+rm(list=ls()); gc()
 adt <- read.csv("Data/AD_Tracking/train_sample.csv", stringsAsFactors = F)
-y <- adt$is_attributed
-tri <- 1:nrow(adt)
 adt$click_time <- as.POSIXct(adt$click_time)
 range(adt$click_time)
 adt$click_hour <- format(adt$click_time, "%H")
@@ -128,8 +127,20 @@ colnames(adt)
 head(adt)
 #install.packages("xgboost")
 library(xgboost)
-?xgb.DMatrix
+library(caret)
+colnames(adt)
+colnames(adt[,-c(6,8)])
+adt_index <- createDataPartition(adt$is_attributed, p=0.7, list = F)
+y <- adt[adt_index,]$is_attributed
 
+adtr <- adt[,-c(6,8)]
+
+dtest <- xgb.DMatrix(data = data.matrix(adtr[-adt_index,]))
+str(dtest)
+tri <- createDataPartition(y, p = 0.9, list = F)
+dtrain <- xgb.DMatrix(data = data.matrix(adtr[adt_index,][tri,]), label = y[tri])
+dval <- xgb.DMatrix(data = data.matrix(adtr[adt_index,][-tri,]), label = y[-tri])
+cols <- colnames(adtr)
 #dtest <- xgb.DMatrix(data = data.matrix(tr_te[-tri]))
 #tr_te <- tr_te[tri]
 #tri <- caret::createDataPartition(y, p = 0.9, list = F)
@@ -139,6 +150,39 @@ library(xgboost)
 #
 #rm(tr_te, y, tri); gc()
 
+p <- list(objective = "binary:logistic",
+          booster = "gbtree",
+          eval_metric = "auc",
+          nthread = 8,
+          eta = 0.07,
+          max_depth = 7,
+          min_child_weight = 148,
+          gamma = 167.6125,
+          subsample = 0.6928,
+          colsample_bytree = 0.9108,
+          colsample_bylevel = 0.9857,
+          alpha = 43.2165,
+          lambda = 74.6334,
+          scale_pos_weight = 103,
+          nrounds = 2000)
+m_xgb <- xgb.train(p, dtrain, p$nrounds, list(val = dval), print_every_n = 50, 
+                   early_stopping_rounds = 200)
 
-args(save)
-example(save)
+(imp <- xgb.importance(cols, model=m_xgb))
+xgb.plot.importance(imp, top_n = 10)
+predXG <- predict(m_xgb,dtest)
+predXG <- ifelse(predXG > 0.7,1,0)
+sum(predXG)
+nrow(adt[-adt_index,])
+library(e1071)
+confusionMatrix(predXG, adt[-adt_index,]$is_attributed)
+
+#install.packages("ROCR")
+library(ROCR)
+pr <- prediction(predXG, adt[-adt_index,]$is_attributed)
+prf <- performance(pr, measure = "tpr", x.measure = "fpr")
+plot(prf)
+
+auc <- performance(pr, measure = "auc")
+auc <- auc@y.values[[1]]
+auc
