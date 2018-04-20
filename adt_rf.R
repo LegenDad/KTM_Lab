c2192b819447507c6b2e935b866a19f8e0c4f550
LegenDad <37681131+LegenDad@users.noreply.github.com>
2018-04-20 23:07:55 +0900

    rf sepearte

--- adt_rf.R
+++ adt_rf.R
@@ -35,49 +35,21 @@ colnames(adte)
 colnames(adtr)
 table(adtr$is_attributed); table(adte$is_attributed)
 str(adtr)
-###### glm #####
-glm <- glm(as.factor(is_attributed)~., family = binomial, adtr)
-summary(glm)
-library(pscl)
-pR2(glm)
-pred_glm <- predict(glm, newdata = adte, type = "response")
-#pred_glm2 <- ifelse(pred_glm > 0.5,1,0)
 library(e1071)
-#confusionMatrix(as.factor(pred_glm2), as.factor(y[-adt_index]))
 library(ROCR)
-pr <- prediction(pred_glm, y[-adt_index])
-prf <- performance(pr, measure = "tpr", x.measure = "fpr")
-plot(prf)
-auc <- performance(pr, measure = "auc")
-auc <- auc@y.values[[1]]
-auc
-
-##### Decision Tree #####
-library(rpart)
-rtree <- rpart(as.factor(is_attributed)~., adtr, method = "class")
-summary(rtree)
-pred_rt <- predict(rtree, adte, type = "class")
-pred_rt2 <- predict(rtree, adte, type = "prob")[,2]
-confusionMatrix(pred_rt,  as.factor(y[-adt_index]))
-pr2 <- prediction(pred_rt2,  y[-adt_index])
-prf2 <- performance(pr, measure = "tpr", x.measure = "fpr")
-plot(prf2)
-auc2 <- performance(pr2, measure = "auc")
-auc2 <- auc2@y.values[[1]]
-auc2
-
 ##### Random Forest #####
 library(randomForest)
 model.rf=randomForest(as.factor(is_attributed)~., adtr)
 model.rf2=randomForest(as.factor(is_attributed)~., adtr, importance = T)
 model.rf
+importance(model.rf2)
 plot(model.rf)
-pred_rf <- predict(model.rf, adte)
-pr3 <- prediction(pred_rf, adte$is_attributed)
-pr3 <- prediction(pred_rf, adte$is_attributed)
-prf3 <- performance(pr3, measure = "tpr", x.measure = "fpr")
-plot(prf3)
+pred_rf <- predict(model.rf, adte,  type = "prob")[,2]
+pred_rf
+pr <- prediction(pred_rf, adte$is_attributed)
+prf <- performance(pr, measure = "tpr", x.measure = "fpr")
+plot(prf)
 
-auc3 <- performance(pr3, measure = "auc")
-auc3 <- auc3@y.values[[1]]
-auc3
+auc <- performance(pr, measure = "auc")
+auc <- auc@y.values[[1]]
+auc
