63cdfb2c10aa9355a5c6e065fe401f17693bda6a
LegenDad <37681131+LegenDad@users.noreply.github.com>
2018-04-22 21:44:09 +0900

    rf model

--- ADT/adt_rf.R
+++ ADT/adt_rf.R
@@ -1,7 +1,6 @@
 rm(list=ls()); gc()
-#adt <- adt[sample(.N, 30e6), ]
 library(data.table)
-adt <- fread("Data/AD_Tracking/train_sample.csv")
+adt <- fread("./ADT/Data/train_sample.csv")
 library(lubridate)
 adt$click_hour <- hour(adt$click_time)
 adt$click_weekd <- wday(adt$click_time)
@@ -24,32 +23,24 @@ colnames(adt)[11:20] <- c("ip_hw", "ip_app", "ip_dev", "ip_os", "ip_ch",
 colnames(adt)
 
 library(caret)
-adt_index <- createDataPartition(adt$is_attributed, p=0.7, list = F)
+adt_index <- createDataPartition(adt$is_attributed, p = 0.7, list = F)
 y <- adt$is_attributed
 adtr <- adt %>% select(-ip, -click_time, -attributed_time)
-
 adte <- adtr[-adt_index,]
 adtr <- adtr[adt_index,]
-
-colnames(adte)
-colnames(adtr)
+colnames(adte) == colnames(adtr)
+rm(adt); gc()
 table(adtr$is_attributed); table(adte$is_attributed)
-str(adtr)
 library(e1071)
 library(ROCR)
-##### Random Forest #####
 library(randomForest)
-model.rf=randomForest(as.factor(is_attributed)~., adtr)
-model.rf2=randomForest(as.factor(is_attributed)~., adtr, importance = T)
+model.rf <- randomForest(as.factor(is_attributed)~., adtr, importance = T)
 model.rf
-importance(model.rf2)
-plot(model.rf)
-pred_rf <- predict(model.rf, adte,  type = "prob")[,2]
-pred_rf
+importance(model.rf)
+pred_rf <- predict(model.rf, adte, type ="prob")[,2]
 pr <- prediction(pred_rf, adte$is_attributed)
-prf <- performance(pr, measure = "tpr", x.measure = "fpr")
+prf <- performance(pr, "tpr", "fpr")
 plot(prf)
-
 auc <- performance(pr, measure = "auc")
 auc <- auc@y.values[[1]]
 auc
